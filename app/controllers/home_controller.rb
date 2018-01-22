f8da6216fa1b271543bde62f43a30ff2df4e026a
Michael Lee <mleenavy@gmail.com>
2018-01-22 10:26:35 -0600

    bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,7 +25,7 @@ class HomeController < ApplicationController
   private
 
     def get_platoon
-      @students = Student.where(platoon: params[:platoon]).where(first_name: true)
+      @students = Student.where(platoon: params[:platoon]).select {|s| s.first_name != nil}
     end
 
 end
